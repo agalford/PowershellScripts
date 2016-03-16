07370be1982e54f04370390423fdd5529cf7f740
Adam Alford <agalford@ncsu.edu>
2016-03-16 10:41:01 -0400

    Create ConnectToExchangeOnline.ps1

---
+++ ConnectToExchangeOnline.ps1
@@ -0,0 +1,10 @@
+#Connecting to Exchange Online
+
+#Capture administrative credential for future connections.
+$credential = get-credential
+
+#Creates an Exchange Online session
+$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
+
+#Import session commands
+Import-PSSession $ExchangeSession
